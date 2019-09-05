52f6746ae6b286d9e17f6e9d472fdbdd148e074f
David Elstob <david.elstob@vidsy.co>
2019-09-05 13:46:23 +0100

    Allow for dynamic hook values

--- lib.js
+++ lib.js
@@ -46,5 +46,5 @@ export default function usePageVisibility(cb = () => {}, delay) {
     return () => {
       document.removeEventListener(visibilityChange, handleVisibilityChange);
     };
-  }, []);
+  }, [cd, delay]);
 }
